e94384c4f16c1cf9d7ac04e9d106e066fc4780bb
darkname04 <moeinaskarpoor83m@gmail.com>
2023-11-17 20:37:05 +0330

    moein

---
+++ 6.py
@@ -0,0 +1,43 @@
+#تمرسن1.6
+x=("moein")
+y=""
+for i in x:
+    y=i+y
+print("تمرین1.6",y)
+
+
+#تمرین2.6
+x=("moein")
+print("تمرین2.6",x.find("m"))
+
+
+#"تمرین3.6"
+b="bfggyioutterdtfhli"
+c=0
+char="t"
+for i in b:
+    if i==char :
+        c+=1
+print(  "ـمرین3.6",c)
+
+
+#4.6تمرین
+def m(x):
+    b=x**2 + 2*x
+    return (b)
+
+def n(j):
+    c=2*j + 2
+    return (c)
+def fx():
+    v=m(5) - m(5) / n(34)
+    print("تمرین4.6",v)
+fx()
+
+#تمرین5.6
+moein=("moein"[::-1])
+print("تمرین5.6",moein)
+
+#تمرین6.6
+
+print("هر چی فکر کردم نشد/:")
